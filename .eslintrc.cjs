0c8fc6d6323f4f1ad8b5134d3ae967c459c7be2a
TOWERMING\vanne <vanness@vip.qq.com>
2024-06-27 19:26:56 +0800

    更新格式插件

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,14 +6,18 @@ module.exports = {
     '@remix-run/eslint-config',
     'plugin:hydrogen/recommended',
     'plugin:hydrogen/typescript',
+    'plugin:prettier/recommended', // 添加这行
+    'prettier' // 确保这一行在最后
   ],
+  plugins: ['prettier'], // 添加这行
   rules: {
+    'prettier/prettier': 'error', // 添加这行
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/naming-convention': 'off',
     'hydrogen/prefer-image-component': 'off',
     'no-useless-escape': 'off',
     '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
     'no-case-declarations': 'off',
-    'no-console': ['warn', {allow: ['warn', 'error']}],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
   },
 };
